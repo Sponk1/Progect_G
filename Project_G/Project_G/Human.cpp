5255f22c303e244980a99fd13f0ae93d1c98c21f
Spon'k <zhludov.work@gmail.com>
2023-03-27 16:43:20 +0300

    human done

--- Project_G/Project_G/Human.cpp
+++ Project_G/Project_G/Human.cpp
@@ -2,12 +2,12 @@
 
 Human::Human()
 {
-	genere = ""; weight = 0; height = 0; typeOfActivity = "";
+	genere = ""; weight = 0; height = 0; typeOfActivity = ""; age = 0; name = "";
 }
 
-Human::Human(string g, double w, double h, string toa)
+Human::Human(string n, string g, double w, double h, string toa, double a)
 {
-	genere = g; weight = w; height = h; typeOfActivity = toa;
+	genere = g; weight = w; height = h; typeOfActivity = toa; age = a; name = n;
 }
 
 // геттеры и сэттеры
@@ -26,3 +26,24 @@ double Human::getHeight() { return height; }
 void Human::setActivity(string toa) { typeOfActivity = toa; }
 
 string Human::getActivity() { return typeOfActivity; }
+
+void Human::setAge(double a) { age = a; }
+
+double Human::getAge() { return age; }
+
+void Human::setName(string n) { name = n; }
+
+string Human::getName() { return name; }
+
+//реализация перешрузки функций ввода и вывода
+
+ostream& operator<<(ostream& os, Human& obj)
+{
+	os << "Ваше имя: " << obj.name << endl;
+	os << "Ваш пол: " << obj.genere << endl;
+	os << "Ваш возраст: " << obj.age << endl;
+	os << "Ваш вес: " << obj.weight << ", ваш рост: " << obj.height << endl;
+	os << "Ваш тип активоности: " << obj.typeOfActivity << endl;
+
+	return os;
+}
--- Project_G/Project_G/Human.h
+++ Project_G/Project_G/Human.h
@@ -9,14 +9,15 @@ class Human
 protected:
 	//параметры человека
 	string genere;			//гендер
+	string name;			//им€
 	double weight;			//вес
 	double height;			//рост
 	string typeOfActivity;	//вид активности(мала€(1200ккл), низка€(1375ккл), умеренна€(1550ккл), т€жела€(1900ккл))
-
+	double age;				//возраст
 public:
 	//конструкторы по умолчани€ю
 	Human();
-	Human(string g, double w, double h, string toa);
+	Human(string n, string g, double w, double h, string toa, double a);
 
 	//получение информыции каждого параметра и его вывод
 	//гнедер
@@ -31,6 +32,16 @@ public:
 	//вид активности
 	void setActivity(string toa);
 	string getActivity();
+	//возраст
+	void setAge(double a);
+	double getAge();
+	//им€
+	void setName(string n);
+	string getName();
+
+	//перегрузка операторов ввода и вывода
+	friend ostream& operator<<(ostream& os, Human& obj);
+	//friend& operator>>(&istream is, &Human obj);
 
 };
 
